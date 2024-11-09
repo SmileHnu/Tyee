ff682e809136bd102dba3b1ac8888d450ecbde3c
Shu Lingyu <674729985@qq.com>
2024-11-09 12:56:24 +0800

    rename & move

--- models/downstream/emotion_recognition_model.py
+++ models/downstream/emotion_recognition_model.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, input_dim=6, hidden_dim=50, output_dim=2):
+    def __init__(self, input_dim=24, hidden_dim=12, classes=4):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)  
         self.relu = nn.ReLU()                        
-        self.fc2 = nn.Linear(hidden_dim, output_dim)  
+        self.fc2 = nn.Linear(hidden_dim, classes)  
 
-    def forward(self, x):
+    def forward(self, x, x_len):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        return x
+        return x, x_len
--- models/wrap_model.py
+++ models/wrap_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class UpstreamDownstreamModel(nn.Module):
+class WrappedMode(nn.Module):
     def __init__(self, upstream, downstream, upstream_trainable):
         super().__init__()
         self.upstream = upstream
